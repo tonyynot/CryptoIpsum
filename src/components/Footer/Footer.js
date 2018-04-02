8406fb21ab04f0c2a5cd9b934c5d191c46130ce8
aramella <aramella@americanbible.org>
2018-04-02 19:22:23 -0400

    add styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,8 +6,9 @@ class FooterComponent extends Component {
   render() {
     return (
         <div className="footer-container">
-          <div className="sub-heading">Created by <a href="http://graysonhicks.com">Anthony Ramella</a></div>
-          <div><a href="https://github.com/graysonhicks/sanctum-ipsum"><FaGithub className="footer-logo" size={30}/></a></div>
+          <div className="sub-heading">Created by <a href="https://tonyramella.me">Anthony Ramella</a></div>
+          <div>Fork this project on Github <a href="https://github.com/tonyynot/cryptoipsum"><FaGithub className="footer-logo" size={30}/></a></div>
+          <p>Donate Bitcoin</p>
         </div>
     );
   }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,8 +6,8 @@ class HeaderComponent extends Component {
   render() {
     return (
         <div className="header-container">
-          <span className="main-header">Crypto Ipsum</span>
-          <span className="header-logo-container"><FaBitcoin className="header-logo" size={30}/></span>
+          <span className="main-header">Crypto Ipsum</span><br></br>
+          <span className="header-logo-container"><FaBitcoin className="header-logo" size={30}/></span><br></br>
           <span className="sub-heading">A Lorem Ipsum text generator for Blockchain nerds.</span>
         </div>
     );
